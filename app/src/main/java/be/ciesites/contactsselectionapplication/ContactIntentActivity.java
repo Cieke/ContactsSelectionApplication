2085bfe7d3d0cc4698479b5b21bee76a6a455f11
Nancy De Smet <ciek.eu@gmail.com>
2016-10-12 21:04:24 +0200

    shows hardcoded list - links to detail page on detail page you can click on URL , will open browser will not yet open phone app when clicking on phone numer

--- app/src/main/java/be/ciesites/contactsselectionapplication/ContactIntentActivity.java
+++ app/src/main/java/be/ciesites/contactsselectionapplication/ContactIntentActivity.java
@@ -1,7 +1,14 @@
 package be.ciesites.contactsselectionapplication;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -30,11 +37,49 @@ public class ContactIntentActivity extends AppCompatActivity {
         contactsList.add(new ContactObject("Android Four", "444111-111", "www.androidATC.com"));
 
         List<String> listName = new ArrayList<String>();
-        for (int i = 0; i < contactsList.size() ; i++) {
+        for (int i = 0; i < contactsList.size(); i++) {
             listName.add(contactsList.get(i).getName());
         }
         adapter = new ArrayAdapter<String>(ContactIntentActivity.this, android.R.layout.simple_expandable_list_item_1, listName);
 
         intentListView.setAdapter(adapter);
+
+        intentListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent i = new Intent(ContactIntentActivity.this, ContactPageActivity.class);
+                i.putExtra("Object", contactsList.get(position));
+                startActivityForResult(i, 0);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (data == null) {
+            return;
+        }
+        Bundle resultData = data.getExtras();
+        String value = resultData.getString("value");
+        switch (resultCode) {
+            case PHONE:
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                startActivity(new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + value)));
+                break;
+            case WEBSITE:
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://" + value)));
+                break;
+        }
+
     }
 }
